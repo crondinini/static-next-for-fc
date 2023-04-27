15174a61e902cd852e144a5419ad6d57da2140ee
Camila Rondinini <crondinini@gmail.com>
2023-04-27 16:05:17 +0100

    mark time in next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Another modification!
+            After 4:05pm
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
           <div>
