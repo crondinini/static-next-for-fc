39740b12920d301fddb8f28a211032466fc4c56f
Camila Rondinini <crondinini@gmail.com>
2023-07-03 19:00:19 +0100

    some changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Second PR - June 05 
+            Changes
           </p>
           <div>
             <a
