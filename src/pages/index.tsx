770593aa9c1c939cab1de4a28e3324487dcdf499
Camila Rondinini <crondinini@gmail.com>
2023-07-19 01:30:12 +0200

    Update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Changes
+            Changes 2
           </p>
           <div>
             <a
